6ce2f06c9ed77162815c75f9046f17cb862029ee
Robotwars1 <magnus.carlsson4@gmail.com>
2023-07-27 16:01:13 +0200

    Feat. Partial parsing and updating of elements

--- SVGColorChanger/SVGColorChanger/MainPage.xaml.cs
+++ SVGColorChanger/SVGColorChanger/MainPage.xaml.cs
@@ -8,6 +8,10 @@ public partial class MainPage : ContentPage
 	public class ColorChanger
 	{
 		public string PlaceholderText { get; set; }
+		public string FromId { get; set; }
+		public string ToId { get; set; }
+		public Color FromColor { get; set; }
+		public Color ToColor { get; set; }
 	}
 
     // Creates Collections of each class to populate each CollectionView
@@ -20,7 +24,7 @@ public partial class MainPage : ContentPage
 
 		mainPage.BackgroundColor = Color.FromRgba("#121212");
 
-		svgColorChanger.Add(new ColorChanger() { PlaceholderText = "Press to type" });
+		svgColorChanger.Add(new ColorChanger() { PlaceholderText = "Press to type", FromId = $"{svgColorChanger.Count}-From", ToId = $"{svgColorChanger.Count}-To" });
         colorInputCollectionView.ItemsSource = svgColorChanger;
     }
 
@@ -45,8 +49,8 @@ public partial class MainPage : ContentPage
 
 	void OnAddColorButtonClicked(object sender, EventArgs e)
 	{
-		svgColorChanger.Add(new ColorChanger() { PlaceholderText = "Press to type" });
-		colorInputCollectionView.ItemsSource = svgColorChanger;
+        svgColorChanger.Add(new ColorChanger() { PlaceholderText = "Press to type", FromId = $"{svgColorChanger.Count}-From", ToId = $"{svgColorChanger.Count}-To" });
+        colorInputCollectionView.ItemsSource = svgColorChanger;
 	}
 
 	void OnRemoveColorButtonClicked(object sender, EventArgs e)
@@ -54,4 +58,55 @@ public partial class MainPage : ContentPage
 		svgColorChanger.RemoveAt(svgColorChanger.Count - 1);
         colorInputCollectionView.ItemsSource = svgColorChanger;
     }
+
+	void OnTextChanged(object sender, TextChangedEventArgs e)
+	{
+		// Dumb workaround to update color of BoxView
+		int a = svgColorChanger.Count;
+		svgColorChanger.Clear();
+		for (int i = 0; i < a; i++)
+		{
+			svgColorChanger.Add(new ColorChanger() { PlaceholderText = "Press to type", FromId = $"{svgColorChanger.Count}-From", ToId = $"{svgColorChanger.Count}-To", FromColor = Colors.Blue });
+		}
+	}
+
+	void OnTextCompleted(object sender, EventArgs e)
+	{
+		// To-Do
+	}
+
+	/*
+	void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+	{
+		Console.WriteLine("aaaaaaaaaa");
+		/*
+        for (int i = 0; i < svgColorChanger.Count; i++)
+		{
+            if (((Editor)sender).ClassId == svgColorChanger[i].FromId)
+			{
+				try
+				{
+                    svgColorChanger[i].FromColor = Color.FromArgb(((Editor)sender).Text);
+                }
+				catch 
+				{
+					// To-Do
+				}
+				
+			}
+			else if (((Editor)sender).ClassId == svgColorChanger[i].ToId)
+			{
+				try
+				{
+                    svgColorChanger[i].ToColor = Color.FromArgb(((Editor)sender).Text);
+                }
+				catch
+				{
+					// To-Do
+				}
+			}
+		}
+		
+	}
+	*/
 }
